d0f1d5c347fb7d09be043b1d183f03a102c0a380
Christian Reinhardt <christian.reinhardt@ics.uzh.ch>
2021-06-07 08:13:48 +0200

    Wrote a function that reads tablegrid.txt.

--- src/testaneoswrapper.c
+++ src/testaneoswrapper.c
@@ -14,7 +14,7 @@
 #define SUCCESS 0
 #define FAIL -1
 
-int ReadTableGrid(char *chFile, int *nRho, int *nT) {
+int ReadTableGrid(char *chFile, double *rho, double *T, int *pnRho, int *pnT) {
     double sesameid;
     double date;
     double version;
@@ -27,55 +27,71 @@ int ReadTableGrid(char *chFile, int *nRho, int *nT) {
     int nT;
     FILE *fp;
     int iRet;
-    
+    int i;
+
+    if (rho != NULL) free(rho);
+    if (T != NULL) free(T);
+
     fp = fopen(chFile, "r");
-    if (fp == NULL)
-        return FAIL;
+    if (fp == NULL) return FAIL;
 
     /* Read the file. */
     iRet = fscanf(fp, "%lf", &sesameid);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &date);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &version);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &fmn);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &fmw);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &rho0);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &K0);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &T0);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
 
-    iRet = fscanf(fp, "%lf", &nRho);
-    if (iRet != 1)
-        return FAIL;
+    iRet = fscanf(fp, "%d", &nRho);
+    if (iRet != 1) return FAIL;
  
-    iRet = fscanf(fp, "%lf", &nT);
-    if (iRet != 1)
-        return FAIL;
+    iRet = fscanf(fp, "%d", &nT);
+    if (iRet != 1) return FAIL;
 
     iRet = fscanf(fp, "%lf", &K0);
-    if (iRet != 1)
-        return FAIL;
+    if (iRet != 1) return FAIL;
+
+    /* Allocate memory and read rho and T. */
+    rho = calloc(nRho, sizeof(double));
+    assert(rho != NULL);
+
+    T = calloc(nT, sizeof(double));
+    assert(rho != NULL);
+
+    for (i=0; i<nRho; i++) {
+        iRet = fscanf(fp, "%lf", &rho[i]);
+        if (iRet != 1) return FAIL;
+    }
+
+    for (i=0; i<nT; i++) {
+        iRet = fscanf(fp, "%lf", &T[i]);
+        if (iRet != 1) return FAIL;
+    }
+
+    fclose(fp);
+
+    *pnRho = nRho;
+    *pnT = nT;
+
+    return SUCCESS;
 }
 
 int main(int argc, char **argv) {
@@ -99,6 +115,19 @@ int main(int argc, char **argv) {
     /* EOS table. */
     int nRho;
     int nT;
+    double *rhoAxis;
+    double *TAxis;
+    int i;
+
+    /* Test if reading the file tablegrid.txt works. */
+    if (!ReadTableGrid("tablegrid.txt", rhoAxis, TAxis, &nRho, &nT)) {
+        fprintf(stderr, "Failed reading tablegrid.txt.\n");
+        exit(1);
+    }
+    
+    fprintf(stderr, "nRho= %i nT= %i\n", nRho, nT);
+    
+    exit(1);
 
     if (argc != 4) {
         fprintf(stderr, "Usage: aneoscall <rho> <T> <iMat>\n");
